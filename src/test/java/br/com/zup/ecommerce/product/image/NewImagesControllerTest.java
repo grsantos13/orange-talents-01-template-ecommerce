f132c92c29bdd90ec4b7c65b618ba7b125aede97
Gustavo Santos <gsantoset@gmail.com>
2021-02-03 10:33:54 -0300

    Organizado os imports

--- src/test/java/br/com/zup/ecommerce/product/image/NewImagesControllerTest.java
+++ src/test/java/br/com/zup/ecommerce/product/image/NewImagesControllerTest.java
@@ -9,7 +9,6 @@ import br.com.zup.ecommerce.user.CleanPassword;
 import br.com.zup.ecommerce.user.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
